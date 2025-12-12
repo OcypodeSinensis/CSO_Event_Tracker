714838a0c2b3d43f389ec59667eff0f1790da305
OcypodeSinensis <tianyue.zhaozty@outlook.com>
2025-12-12 03:58:56 -0500

    Add dashboard, empty leaflet map and chartjs chart

--- cso_app/urls.py
+++ cso_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('report/', views.report_event, name='report_event'),
     path('report/success/', views.report_success, name='report_success'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- cso_app/views.py
+++ cso_app/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render, redirect
 from .forms import CSOEventForm
+from .models import CSOEvent
 
 def report_event(request):
     if request.method == "POST":
         form = CSOEventForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('report_success')  # redirect after submitting
+            return redirect('report_success')
     else:
         form = CSOEventForm()
 
     return render(request, 'report_event.html', {'form': form})
 
-
 def report_success(request):
     return render(request, 'report_success.html')
+
+def dashboard(request):
+    events = CSOEvent.objects.all()
+
+    event_data = [
+        {
+            "location": event.location,
+            "severity": event.severity,
+            "volume": event.overflow_volume_m3,
+            "start_time": event.start_time.strftime("%Y-%m-%d %H:%M"),
+        }
+        for event in events
+    ]
+
+    return render(request, 'dashboard.html', {"events": event_data})
 
