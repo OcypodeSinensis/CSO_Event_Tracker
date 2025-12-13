ce4d0788ace7b549cdc759b64c5f429c01d52f30
OcypodeSinensis <tianyue.zhaozty@outlook.com>
2025-12-12 20:58:43 -0500

    Fixed url connection for mitigation action

--- cso_app/urls.py
+++ cso_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('report/success/', views.report_success, name='report_success'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path("export/xlsx/", export_events_xlsx, name="export_xlsx"),
+    path('action/add/', views.add_action, name='add_action'),
+    path('action/success/', views.action_success, name='action_success'),
 ]
